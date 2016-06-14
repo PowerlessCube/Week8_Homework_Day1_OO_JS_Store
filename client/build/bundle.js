dd37a7e443fc3444a3f7efccf07cbfc5c8631626
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-06-14 09:11:28 +0100

    homework commit

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -16583,8 +16583,11 @@
 
 /***/ },
 /* 6 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
+	var Record = __webpack_require__( 2 );
+	var RecordStore = __webpack_require__( 1 );
+	
 	var RecordStoreViewer = function( recordStore ) {
 		this.recordStore = recordStore;
 	};
@@ -16608,12 +16611,27 @@
 				recordListInventory.innerText = "Artist: " + record.artist + ", Album: " + record.album + ", price: £" + record.price.toFixed(2);
 				inventoryDisplay.appendChild( recordListInventory );
 			}
+	
+			var addRecord = function( recordStore, newRecord ) {
+				recordStore.addRecordToInventory( newRecord );
+			};
+	
+			// XXX: 3 Further - Add a FORM to add a new Record to the RecordStore.
+			var button = document.getElementById("add-record");
+			button.onclick = function( event ) {
+				var textArtistInput = document.getElementById("artist-input").value;
+				var textAlbumInput = document.getElementById("album-input").value;
+				var textPriceInput = document.getElementById("price-input").value;
+	
+				var newRecord = new Record( textArtistInput, textAlbumInput, textPriceInput );
+	
+				addRecord( recordStore, newRecord );
+			};
 		},
 	
 		storeRecords: function( records ) {
 			localStorage.setItem("storedRecords", JSON.stringify( Records ));
 		}
-		// TODO: 3 Further - Add a FORM to add a new Record to the RecordStore.
 	
 		// TODO: 4 Further - Add ability to sell record
 	
--- client/src/record_store/record_store_viewer.js
+++ client/src/record_store/record_store_viewer.js
@@ -1,3 +1,6 @@
+var Record = require( "./record" );
+var RecordStore = require( "./record_store" );
+
 var RecordStoreViewer = function( recordStore ) {
 	this.recordStore = recordStore;
 };
@@ -21,12 +24,27 @@ RecordStoreViewer.prototype = {
 			recordListInventory.innerText = "Artist: " + record.artist + ", Album: " + record.album + ", price: £" + record.price.toFixed(2);
 			inventoryDisplay.appendChild( recordListInventory );
 		}
+
+		var addRecord = function( recordStore, newRecord ) {
+			recordStore.addRecordToInventory( newRecord );
+		};
+
+		// XXX: 3 Further - Add a FORM to add a new Record to the RecordStore.
+		var button = document.getElementById("add-record");
+		button.onclick = function( event ) {
+			var textArtistInput = document.getElementById("artist-input").value;
+			var textAlbumInput = document.getElementById("album-input").value;
+			var textPriceInput = document.getElementById("price-input").value;
+
+			var newRecord = new Record( textArtistInput, textAlbumInput, textPriceInput );
+
+			addRecord( recordStore, newRecord );
+		};
 	},
 
 	storeRecords: function( records ) {
 		localStorage.setItem("storedRecords", JSON.stringify( Records ));
 	}
-	// TODO: 3 Further - Add a FORM to add a new Record to the RecordStore.
 
 	// TODO: 4 Further - Add ability to sell record
 
